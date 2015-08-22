db6ea8073911bf96995ab335d8c912df44ad29ec
waco001 <abhishek.gorti@gmail.com>
2015-08-22 16:50:40 -0400

    Site for today

--- wsgi/run.py
+++ wsgi/run.py
@@ -10,11 +10,15 @@ app.config['SERVER_NAME']="abhishek.gorti.me"
 def index():
     content="Hello. <br>This is Abhishek Gorti's personal website. <br>There will definitely be more to come. <br>Please check back often. <br>More links will be in the menu(:hover) <br>-waco001 6/23/15 <br>If you need me, add me on skype:f18fan"
     return render_template('index.html', contentraw=content)
+@app.route("/about")
+def about():
+    content="About Me - Nothing Yet!"
+    return render_template('index.html', contentraw=content)
+@app.route("/blog")
+def blog():
+    content="No entries Yet!"
+    return render_template('index.html', contentraw=content)
 @app.route("/contact")
 def contact():
 	content="Call Me.<br>312.725.0351<br>Email Me<br>abhishek.gorti@gmail.com"
-	return render_template('index.html', contentraw=content)
-@app.route("/portfolio", subdomain='foo')
-def portfolio():
-	content="More Coming Soon"
 	return render_template('index.html', contentraw=content)
