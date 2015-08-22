03631ebd58092161b2083331a81f3eb2145a8604
waco001 <abhishek.gorti@gmail.com>
2015-08-22 17:06:43 -0400

    Changes to raw-content

--- wsgi/run.py
+++ wsgi/run.py
@@ -8,7 +8,7 @@ app.config['SERVER_NAME']="abhishek.gorti.me"
 #Create our index or root / route
 @app.route("/")
 def index():
-    content="Hello. <br>This is Abhishek Gorti's personal website. <br>There will definitely be more to come. <br>Please check back often. <br>More links will be in the menu(:hover) <br>-waco001 6/23/15 <br>If you need me, add me on skype:f18fan"
+    content="Hello. <br>This is Abhishek Gorti's personal website. <br>There will definitely be more to come. <br>Please check back often. <br>More links will be in the menu(:hover) <br>-waco001 8/22/15"
     return render_template('index.html', contentraw=content)
 @app.route("/about")
 def about():
@@ -20,5 +20,5 @@ def blog():
     return render_template('index.html', contentraw=content)
 @app.route("/contact")
 def contact():
-	content="Call Me.<br>312.725.0351<br>Email Me<br>abhishek.gorti@gmail.com"
+	content="Call Me.<br>312.725.0351<br>Email Me<br>abhishek.gorti@gmail.com<br>skype:f18fan"
 	return render_template('index.html', contentraw=content)
