050ff353f386af26f2d05b45685fecffa7d81755
waco001 <abhishek.gorti@gmail.com>
2015-08-22 16:15:15 -0400

    Testing subdomain

--- wsgi/run.py
+++ wsgi/run.py
@@ -4,20 +4,17 @@ from flask import render_template
 from flask import request
 
 app = Flask(__name__)
-
+app.config['SERVER_NAME']="abhishek.gorti.me"
 #Create our index or root / route
 @app.route("/")
 def index():
     content="Hello. <br>This is Abhishek Gorti's personal website. <br>There will definitely be more to come. <br>Please check back often. <br>More links will be in the menu(:hover) <br>-waco001 6/23/15 <br>If you need me, add me on skype:f18fan"
-    return render_template('index.html', contentraw=content, guitar=False)
+    return render_template('index.html', contentraw=content)
 @app.route("/contact")
 def contact():
 	content="Call Me.<br>312.725.0351<br>Email Me<br>abhishek.gorti@gmail.com"
 	return render_template('index.html', contentraw=content)
-@app.route("/portfolio")
+@app.route("/portfolio", subdomain='foo')
 def portfolio():
 	content="More Coming Soon"
 	return render_template('index.html', contentraw=content)
-
-if __name__ == "__main__":
-    app.run(debug = "True",host='0.0.0.0',port=8000)
