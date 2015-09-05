662736c8089c9a0a77c31f33ac37e8afa65f844a
waco001 <abhishek.gorti@gmail.com>
2015-09-05 19:21:51 -0400

    A

--- wsgi/run.py
+++ wsgi/run.py
@@ -2,27 +2,12 @@ import os
 from flask import Flask
 from flask import render_template
 from flask import request
-
+from abhishekgorti import abhishekgorti
 app = Flask(__name__)
 app.config['SERVER_NAME']="gorti.me"
+app.register_blueprint(abhishekgorti)
 #Create our index or root / route
 @app.route("/")
 def index():
-    content="Hello. <br>This is Abhishek Gorti's personal website. <br>There will definitely be more to come. <br>Please check back often. <br>More links will be in the menu(:hover) <br>-waco001 8/22/15"
-    return render_template('index.html', contentraw=content)
-@app.route("/about")
-def about():
-    content="About Me - Nothing Yet!"
-    return render_template('index.html', contentraw=content)
-@app.route("/blog")
-def blog():
-    content="No entries Yet!"
-    return render_template('index.html', contentraw=content)
-@app.route("/contact")
-def contact():
-	content="Call Me.<br>312.725.0351<br>Email Me<br>abhishek.gorti@gmail.com<br>skype:f18fan"
-	return render_template('index.html', contentraw=content)
-
-@app.route('/',subdomain="abhishek")
-def abhishek():
-    return "ABHISHEK GORTI!"
+    content="Hello, you must be looking for Abhishek's Website: <a href='abhishek.gorti.me'>abhishek.gorti.me</a>"
+    return content
