596a5a60ac7b9c1ebab9f124d3f4ec806238010c
waco001 <abhishek.gorti@gmail.com>
2015-09-10 09:27:57 -0400

    Updated about, homepage, menu

--- wsgi/abhishekgorti.py
+++ wsgi/abhishekgorti.py
@@ -4,15 +4,11 @@ abhishekgorti = Blueprint('abhishekgorti',__name__,subdomain='abhishek')
 
 @abhishekgorti.route("/")
 def index():
-    content="Hello. <br>This is Abhishek Gorti's personal website. <br>There will definitely be more to come. <br>Please check back often. <br>More links will be in the menu(:hover) <br>-waco001 8/22/15"
-    return render_template('index.html', contentraw=content)
-@abhishekgorti.route("/about")
-def about():
-    content="About Me - Nothing Yet!"
+    content="Hi!<br>My name is Abhishek Gorti. I am a student at Hamden High School.<br>A few things about me:<br><ul><li>I am addicted to programming</li><li>I love aviation and am a student pilot.</li><li>I need to know how everything works.</li></ul><br>You can navigate by hovering over the menu bar above.<br><a href='http://gorti.me/sitelist'>Sitelist</a>"
     return render_template('index.html', contentraw=content)
 @abhishekgorti.route("/blog")
 def blog():
-    content="No entries Yet!"
+    content="Timeline:<br>This is not my blog, but this is where I will post my programming projects.<br>The internet has a new form of art for people like me. I'll make cool works that will demonstrate my skill of design, function, and art.<br>I currently working on a project. It will be done around October. Check back then."
     return render_template('index.html', contentraw=content)
 @abhishekgorti.route("/contact")
 def contact():
--- wsgi/run.py
+++ wsgi/run.py
@@ -6,7 +6,7 @@ from abhishekgorti import abhishekgorti
 app = Flask(__name__)
 app.register_blueprint(abhishekgorti)
 PAGE_LAST_MODIFIED = '2015-09-06'
-app.config['SERVER_NAME']="gorti.me"
+#app.config['SERVER_NAME']="gorti.me"
 #Create our index or root / route
 
 @app.route("/",subdomain='www')
@@ -14,6 +14,7 @@ app.config['SERVER_NAME']="gorti.me"
 def index():
     content="Hello, you must be looking for Abhishek's Website: <a href='http://abhishek.gorti.me'>abhishek.gorti.me</a>"
     return content
+@app.route('/sitemap',methods=['GET'])
 @app.route('/sitemap.xml',methods=['GET'])
 def sitemap():
     """Generate sitemap.xml """
