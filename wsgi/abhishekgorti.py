74fedd86881be780b7e9f2914b1770fe7545c5b5
waco001 <abhishek.gorti@gmail.com>
2015-09-10 09:30:55 -0400

    Forgot to update timeline page

--- wsgi/abhishekgorti.py
+++ wsgi/abhishekgorti.py
@@ -6,7 +6,7 @@ abhishekgorti = Blueprint('abhishekgorti',__name__,subdomain='abhishek')
 def index():
     content="Hi!<br>My name is Abhishek Gorti. I am a student at Hamden High School.<br>A few things about me:<br><ul><li>I am addicted to programming</li><li>I love aviation and am a student pilot.</li><li>I need to know how everything works.</li></ul><br>You can navigate by hovering over the menu bar above.<br><a href='http://gorti.me/sitelist'>Sitelist</a>"
     return render_template('index.html', contentraw=content)
-@abhishekgorti.route("/blog")
+@abhishekgorti.route("/timeline")
 def blog():
     content="Timeline:<br>This is not my blog, but this is where I will post my programming projects.<br>The internet has a new form of art for people like me. I'll make cool works that will demonstrate my skill of design, function, and art.<br>I currently working on a project. It will be done around October. Check back then."
     return render_template('index.html', contentraw=content)
